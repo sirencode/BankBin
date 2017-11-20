8befe59f779361330cca192256afb63c7db2832e
Diablo <shenyonghe525@gmail.com>
2017-11-20 22:07:34 +0800

    bank bin

---
+++ app/src/main/java/com/example/diablo/bankbin/SecoundAct.java
@@ -0,0 +1,33 @@
+package com.example.diablo.bankbin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Diablo on 2017/11/20.
+ */
+
+public class SecoundAct extends Activity {
+
+    private EditText editText;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edit_num);
+        textView = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(v -> {
+            if (!TextUtils.isEmpty(editText.getText())) {
+                long start = System.currentTimeMillis();
+                String bankNum = editText.getText().toString();
+                textView.setText(BankBinCode.getBank(bankNum) + "time" + String.valueOf(System.currentTimeMillis() - start));
+            }
+        });
+    }
+}
